9d86bc594268bda17e034555cbb81f6abfbbf2f9
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-05-27 09:10:22 +0800

    ok

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
 
+const result = Number(5);
 
-function chunk(array = [], chunkSize = 0) {
-    const clone = array.slice();
-    const chunks = [];
-    while (clone.length) chunks.push(clone.splice(0, chunkSize));
-    return chunks;
-}
-
-const array = [1,3,2,5,6,2,6,2,6,1,7,1,6,1,8,37,2,4,1,6,2,7,2];
-const size = 5;
-
-const result = chunk(array, size);
-
-console.log(result);
+console.log(typeof result);
