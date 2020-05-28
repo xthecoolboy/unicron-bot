0f15fbe65ad4c2df97dbb1f3d8deccaed992128a
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-05-28 09:14:45 +0800

    Testtt

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-const deleteElementFromArrayJSON = function (arr, key) {
-    return arr.filter((item) => { return item !== key });
-};
-const data = {
-    arr: [
-        "a",
-        "b",
-        "c",
-        "d",
-    ]
-}
-const results = deleteElementFromArrayJSON(data.arr, 'c');
+/**
+ * PLAYGROUND
+ */
