76d338b3650a5bfdae8d8c434a1aea7d5b3eab26
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-05-28 23:02:25 +0800

    forgot to require ;p

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,6 +2,7 @@ module.exports = (client) => {
     require('../listeners')(client);
     require('./Permissions')(client);
     require('./Emote')(client);
+    require('./Crates')(client);
     require('./ShopItems'(client));
     require('./Commands')(client);
     require('./Events')(client);
