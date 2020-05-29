81c2c5a5836000bd53fd70e59d2ab0e31aded37f
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-05-29 22:48:07 +0800

    swearFilter+

--- src/filters/swearFilter.js
+++ src/filters/swearFilter.js
@@ -1,9 +1,7 @@
 
 const { Client, Message, MessageEmbed } = require('discord.js');
 
-const fs = require('fs');
-const swearWords = fs.readFileSync('assets/swearWords.txt').toString().split('\r\n');
-const swearRegex = fs.readFileSync('assets/swearRegex.txt').toString().split('\r\n');
+const fs = require('fs').promises;
 
 const reEscape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 
@@ -14,17 +12,19 @@ const reEscape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 module.exports = (client, message) => {
     return new Promise(async (resolve, reject) => {
         try {
+            const swearWords = await fs.readFile('assets/swearWords.txt').toString().split('\r\n');
+            const swearRegex = await fs.readFile('assets/swearRegex.txt').toString().split('\r\n');
             const status = await message.guild.db.filters('swearFilter');
-            const strat = (status && !message.channel.nsfw && message.author.permLevel < 3 && 
+            const strat = (status && !message.channel.nsfw && message.author.permLevel < 3 &&
                 (
                     message.content.match(new RegExp(swearWords.map(reEscape).join('|'), 'gim')) ||
                     message.content.match(new RegExp(swearRegex.map(reEscape)), 'gim')
                 )
-            ) ? true: false;
+            ) ? true : false;
             if (!strat) return resolve(false);
             if (message.deletable) message.delete();
             await message.channel.send(`No Swearing! ${message.author}.`)
-                .then(msg => msg.delete({ timeout: 5000}));
+                .then(msg => msg.delete({ timeout: 5000 }));
             const mChannel = message.guild.channels.resolve(await message.guild.db.moderation('modLogChannel'));
             if (mChannel) {
                 mChannel.send(new MessageEmbed()
