2ae008009b54d8c7fe4ead20c6f7083176247de6
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-01 15:55:14 +0800

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
 require('dotenv').config();
 
+if (process.env.BOT_HOST_URL) {
+    const http = require("http");
+    const express = require("express");
+    const app = express();
+    app.get("/", (request, response) => {
+        console.log(Date.now() + " ping Received");
+        response.sendStatus(200);
+    });
+    app.listen(4200);
+    setInterval(() => {
+        http.get(process.env.BOT_HOST_URL);
+    }, 280000);
+    app.get('/', (request, response) => {
+        response.sendStatus(200);
+    });
+}
+
 require('./prototypes/Array');
 require('./prototypes/Number');
 require('./prototypes/Object');
@@ -9,8 +26,8 @@ require('./prototypes/String');
 const Discord = require('discord.js');
 const { Unicron } = require('./handlers/Unicron');
 
-const client = new Discord.Client({ 
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'] 
+const client = new Discord.Client({
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
 client.unicron = new Unicron({
     owner: process.env.BOT_OWNER_ID,
