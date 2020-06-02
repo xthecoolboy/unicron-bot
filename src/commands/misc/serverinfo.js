0b41b10aae42b3dade20335780a4e95f6f958eba
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-02 18:45:42 +0800

    added few commands

--- src/commands/misc/serverinfo.js
+++ src/commands/misc/serverinfo.js
@@ -9,7 +9,22 @@ module.exports = {
      * @param {Array} args Arguments
      */
     run: async function (client, message, args) {
-
+        message.guild.emojis.cache.size
+        return message.channel.send(new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor(`${message.guild.name} / ${message.guild.id}`)
+            .setImage(message.guild.splash ? message.guild.splashURL() : null)
+            .setThumbnail(message.guild.icon ? message.guild.iconURL() : `https://dummyimage.com/128/7289DA/FFFFFF/&text=${encodeURIComponent(message.guild.nameAcronym)}`)
+            .addField('Owner', `${message.guild.owner.user.tag} / ${message.guild.ownerID}`)
+            .addField('Created At', message.guild.createdAt.toUTCString())
+            .addField('Region', message.guild.region.toUpperCase(), true)
+            .addField('Members', message.guild.memberCount, true)
+            .addField('Emojis', message.guild.emojis.cache.size, true)
+            .addField('Channel Categories', message.guild.channels.cache.filter(channel => channel.type === 'category').size, true)
+            .addField('Text Channels', message.guild.channels.cache.filter(channel => channel.type === 'text').size, true)
+            .addField('Voice Channels', message.guild.channels.cache.filter(channel => channel.type === 'voice').size, true)
+            .setTimestamp()
+        );
     },
     config: {
         name: 'serverinfo',
--- src/commands/misc/userinfo.js
+++ src/commands/misc/userinfo.js
@@ -9,7 +9,44 @@ module.exports = {
      * @param {Array} args Arguments
      */
     run: async function (client, message, args) {
-
+        let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+        if (!user) user = message.author;
+        let member = message.guild.members.cache.get(user.id);
+        let nick = member.nickname;
+        if (!nick) nick = '-';
+        let status = user.presence.status;
+        switch (status) {
+            case 'online': {
+                status = `${await client.getEmoji('online')} Online`;
+                break;
+            }
+            case 'idle': {
+                status = `${await client.getEmoji('idle')} Idle`;
+                break;
+            }
+            case 'dnd': {
+                status = `${await client.getEmoji('dnd')} Do not Disturb`;
+                break;
+            }
+            case 'offline': {
+                status = `${await client.getEmoji('offline')} Offline`;
+                break;
+            }
+        }
+        let roles = member.roles.cache.map(r => `<@&${r.id}>`).slice(1).join(', ').replace(new RegExp(`<@${message.guild.id}>`, 'g'), '');
+        if (roles.length === 0) roles = '-';
+        return message.channel.send(new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setThumbnail(user.displayAvatarURL())
+            .addField(`${user.bot ? 'Bot' : 'User'} Info`, `${user.tag} / ${user.id}`)
+            .addField(`Joined Server`, member.joinedAt.toUTCString(), true)
+            .addField(`Created At`, user.createdAt.toUTCString(), true)
+            .addField('\u200b', '\u200b',true)
+            .addField(`Status`, status, true)
+            .addField(`Nickname`, nick, true)
+            .addField(`Roles`, roles)
+            .setTimestamp()
+        );
     },
     config: {
         name: 'userinfo',
