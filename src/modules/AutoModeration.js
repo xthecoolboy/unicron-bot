aca1e4b88c88ecbd0970c2dc20e0791a3e28094a
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-05 16:31:03 +0800

    AutoModeration module 20%

---
+++ src/modules/AutoModeration.js
@@ -0,0 +1,15 @@
+
+const { Client, Message, MessageEmbed } = require('discord.js');
+const Guild = require('../handlers/Guild');
+const Member = require('../handlers/Member');
+
+/**
+ * @param {Client} client
+ * @param {Message} Message
+ * @param {JSON} data
+ */
+module.exports = (client, message, data) => {
+    return new Promise((resolve, reject) => {
+        
+    });
+}
