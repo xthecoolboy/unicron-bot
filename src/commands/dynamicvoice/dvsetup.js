b2ec9141c8ad29611b444f7278573fa53f7213f0
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-09 15:11:26 +0800

    some stuffs

--- src/commands/dynamicvoice/dvsetup.js
+++ src/commands/dynamicvoice/dvsetup.js
@@ -9,19 +9,20 @@ module.exports = {
      * @param {Array} args Arguments
      */
     run: async function (client, message, args) {
-        const response = await client.awaitReply(message, 'Please select a name for the category of which to setup the dynamic voice channels in\nType `cancel` to cancel this command', 20000, true) //Asks for a category to set DVC In
-        if(!response) return message.channel.send('No response was given, canceling command...'); //Stops execution if no response
-        if(response.content === 'cancel') return message.channel.send('Command was canceled...'); //Stops execution if command cancel is run
-        const channel = await message.guild.channels.create(response.content, {type: "category"});
-        let response1 = await client.awaitReply(message, 'Please select a name for a channel of which to create the dynamic voice channels in\nType `cancel` to cancel this command', 20000, true) //Asks for a category to set DVC In
-        if(!response1) return message.channel.send('No response was given, canceling command...') //Stops execution if no response
-        if(response1.content === 'cancel') return message.channel.send('Command was canceled...') //Stops execution if command cancel is run
-        const chan = await message.guild.channels.create(`${response1}`, {type: 'voice', parent: channel});
+        const response = await client.awaitReply(message, 'Please select a name for the category of which to setup the dynamic voice channels in\n\nType `cancel` to cancel this command', 20000, true) //Asks for a category to set DVC In
+        if (!response) return message.channel.send('No response was given, Exiting setup...'); //Stops execution if no response
+        if (response.content === 'cancel') return message.channel.send('Command was canceled...'); //Stops execution if command cancel is run
+        const channel = await message.guild.channels.create(response.content, { type: "category" });
+        let response1 = await client.awaitReply(message, 'Please select a name for a channel of which to create the dynamic voice channels in\n\nType `cancel` to cancel this command', 20000, true) //Asks for a category to set DVC In
+        if (!response1) return message.channel.send('No response was given, Exiting setup...') //Stops execution if no response
+        if (response1.content === 'cancel') return message.channel.send('Command was canceled...') //Stops execution if command cancel is run
+        const chan = await message.guild.channels.create(`${response1}`, { type: 'voice', parent: channel });
         const model = await message.guild.db.dynamicVoice(true);
         model.category = channel.id;
         model.waitingRoom = chan.id;
         model.enabled = true;
         await model.save();
+        message.channel.send('Dynamic Voice setup, success!');
     },
     config: {
         name: 'dvsetup',
