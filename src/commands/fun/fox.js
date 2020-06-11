30bd6d9ad850b96870aaa055893f9b833b99ecdb
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-11 12:18:14 +0800

    fox

---
+++ src/commands/fun/fox.js
@@ -0,0 +1,36 @@
+
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    /**
+     * 
+     * @param {Discord.Client} client Client
+     * @param {Discord.Message} message Message
+     * @param {Array} args Arguments
+     */
+    run: async function (client, message, args) {
+        const response = await fetch('https://randomfox.ca/floof/');
+        const { image } = await response.json();
+        message.channel.send(new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription('[Random Fox](https://randomfox.ca/)')
+            .setImage(image)
+        );
+    },
+    config: {
+        name: 'fox',
+        description: 'Random pictures of a fox',
+        permission: 'User',
+    },
+    options: {
+        aliases: [],
+        clientPermissions: [],
+        cooldown: 3,
+        nsfwCommand: false,
+        args: false,
+        usage: '',
+        donatorOnly: false,
+        premiumServer: false,
+    }
+}
