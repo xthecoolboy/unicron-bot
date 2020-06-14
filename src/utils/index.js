2febdc15ed35bc88bbf0b1c05796a934176d586a
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-14 21:44:50 +0800

    deleted some unused utilites

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,6 @@
 
 module.exports = {
-    Colors: require('./Colors'),
     Logger:  require('./Logger'),
-    Mutex: require('./Mutex'),
     Random: require('./Random'),
     Regex: require('./Regex'),
     Crypto: require('./Crypto'),
